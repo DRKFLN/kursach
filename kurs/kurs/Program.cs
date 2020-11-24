b902573d785f2de8aec34681e94dc74f53fb96ed
DRKFLN <arata.oni16@gmail.com>
2020-11-24 17:35:01 +0300

    1

---
+++ kurs/kurs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kurs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
